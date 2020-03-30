8ae2d176c69869cebf1ddc9f4196b34248a5ec9d
Joshua Tenner <tenner.joshua@gmail.com>
2020-03-30 00:20:44 -0400

    convert crc32

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-let table = new Uint8Array(256);
-let i = 0;
+let table = new Uint32Array(256);
+let c;
+let i;
+let j;
 
-for (; i < 256; ++i) {
-  table[i] = ((n) => {
-    let r = n;
-    let s = 7;
-    for (n >>>= 1; n; n >>>= 1) {
-      r <<= 1;
-      r |= n & 1;
-      --s;
-    }
-    return ((r << s) & 0xff) >>> 0;
-  })(i);
+for (i = 0; i < 256; ++i) {
+  c = i;
+  for (j = 0; j < 8; ++j) {
+    c = c & 1 ? 0xedb88320 ^ (c >>> 1) : c >>> 1;
+  }
+  table[i] = c >>> 0;
 }
+
 process.stdout.write("[\n");
 for (let i = 0; i < table.length; i++) {
   if (i % 8 === 0) process.stdout.write(" ");
   process.stdout.write(
-    ` 0x${table[i].toString(16).padStart(2, "0").toUpperCase()},`,
+    ` 0x${table[i].toString(16).padStart(8, "0").toUpperCase()},`,
   );
   if (i % 8 === 7) process.stdout.write("\n");
 }
